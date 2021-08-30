9c0724c74818444ac82fff7b7f64bf4ba881f302
elikemowusu <owusuderricke@gmail.com>
2021-08-30 08:12:36 -0700

    Loops and Variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+/* File: 7-print_tebahpla.c
+ * Auth: Derrick Owusu
+ */
+
+#include <stdio.h>
+
+/**
+ * main print the lowercase alphabets in reverse.
+ *
+ *Return: Always 0.
+ */
+
+int main (void)
+{
+  char letter;
+
+  for (letter = 'z'; letter >= 'a'; letter--)
+
+    putchar(letter);
+
+  putchar('\n');
+
+  return (0);
+}
