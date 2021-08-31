835dea9528c567b2cc56ee04c45c433ebedf842f
elikemowusu <owusuderricke@gmail.com>
2021-08-31 13:06:55 -0700

    file 1 Upd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,16 @@
  * Main - prints the alphabet, in lowercase, followed by a new line.
  * Return: Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet();
+
 char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
+
+{
 putchar(letter);
-
+}
+ 
 putchar('\n');
-
-return (0);
 }
