959ab439de1b2162a47c21171a974224cd210072
elikemowusu <owusuderricke@gmail.com>
2021-09-15 13:45:39 -0700

    Upated file 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,109 +1,50 @@
 #include "main.h"
 
-int bandersnatch(char *s1, char *s2);
-char *move(char *s2);
-
+int recursive_check(char *s1, char *s2, int wildC);
 /**
- * wildcmp - compares two strings recursively,
- * checking for wildcards expansion
- * @s1: first string to compare
- * @s2: second string to compare
+ * wildcmp - compares two strings, supports wildcard '*'
  *
- * Return: 1 if the strings can be considered identical
- * otherwise 0
+ * @s1: string 1 to compare to s2
+ * @s2: string 2 to compare to s1
+ *
+ * Return: 1 if identical, 0 otherwise, -1 for error
  */
 int wildcmp(char *s1, char *s2)
 {
-/**
- * this is going to be a sum of return values
- */
-int sum = 0;
-
-/**
- * if we reach the end of s1 and the char in s2 is a *
- * and if the next chars of s2 are *, return 1
- */
-if (*s1 == '\0' && *s2 == '*' && !*move(s2))
-return (1);
-
-/**
- * if the chars are equal in both strings,
- * if we reached the end of s1, return 1
- * else increment s1 and s2 by 1
- */
-if (*s1 == *s2)
-{
-if (*s1 == '\0')
-return (1);
-return (wildcmp(s1 + 1, s2 + 1));
+return (recursive_check(s1, s2, 0));
 }
 /**
- * if we reached the end of both strings,
- * return 0
- */
-if (*s1 == '\0' || *s2 == '\0')
-return (0);
-
-/**
- * if the char in s2 is a *
- * finds the address of the first char after the *
- * if we reached the end of s2, return 1
- * if the chars are equal, add the return values
- * of wildcmp() to sum
- * add the return value of bandersnatch() to sum
- * convert non-zero to 1, keeps 0 at 0, return
- */
-if (*s2 == '*')
-{
-s2 = move(s2);
-if (*s2 == '\0')
-return (1);
-if (*s1 == *s2)
-sum += wildcmp(s1 + 1, s2 + 1);
-sum += bandersnatch(s1 + 1, s2);
-return (!!sum);
-}
-return (0);
-}
-
-/**
- * bandersnatch - checks recursively for all the paths when the
- * characters are equal
- * @s1: first string
- * @s2: second string
+ * recursive_check - checks for wild cards recursively
  *
- * Return: return value of wildcmp() or of itself
- */
-int bandersnatch(char *s1, char *s2)
-{
-/**
- * if we reached the end of s1, return 0
- * if chars are equal, return the return value of wildcmp()
- * increment s1 by 1, not s2
- */
-if (*s1 == '\0')
-return (0);
-if (*s1 == *s2)
-return (wildcmp(s1, s2));
-return (bandersnatch(s1 + 1, s2));
-}
-
-/**
- * *move - moves the current char past the *
- * @s2: string to iterate over
+ * @s1: string 1 to check (no wild cards)
+ * @s2: string to to check (can contain wildcards)
+ * @wildC: counter to see if wildcard is present
  *
- * Return: the address of the character after the *
+ * Return: 1 if identical, 0 for not, -1 for error
  */
-char *move(char *s2)
+int recursive_check(char *s1, char *s2, int wildC)
 {
-/**
- * if the current char is a *
- * increment s2 by 1
- * else return the address of
- * the first char past all *
- */
+if (*s1 == '\0' && *s2 == '\0')
+{
+return (1);
+}
 if (*s2 == '*')
-return (move(s2 + 1));
-else
-return (s2);
+return (recursive_check(s1, s2 + 1, ++wildC));
+else if (*s1 != *s2 && wildC > 0) /* skip to next rel char */
+{
+if (!*s1)
+return (0);
+return (recursive_check(s1 + 1, s2, wildC));
+}
+else if (*s1 == *s2 && wildC == 0) /* char match, next char for each str */
+return (recursive_check(s1 + 1, s2 + 1, 0));
+else if (*s1 == *s2 && wildC > 0)
+{
+if (!recursive_check(s1 + 1, s2 + 1, 0))
+return (recursive_check(s1 + 1, s2, wildC));
+return (recursive_check(s1 + 1, s2 + 1, wildC));
+}
+else if (*s1 != *s2 && wildC == 0) /* non matching and no wildC */
+return (0);
+return (-1); /* something unexpected occured, return -1 for error */
 }
