42867c68c5a56b05c941664a0a41f15c05a0647e
elikemowusu <owusuderricke@gmail.com>
2021-09-19 03:48:53 -0700

    Updt

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,32 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - multiplies two arguments and prints result
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if no errors
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
  */
 
 int main(int argc, char *argv[])
 {
-int i, first_num, sec_num, product;
+
+int prod;
 
 if (argc != 3)
 {
-return (printf("Erro\n"), 1);
+
+printf("Error\n");
+return (1);
+
 }
-for (i = 0; i < argc; i++)
+else
 {
-first_num = atoi(argv[1]);
-sec_num = atoi(argv[2]);
-}
-product = first_num * sec_num;
-printf("%d\n", product);
+prod = (atoi(argv[1]) * atoi(argv[2]));
+printf("%d\n", prod);
 return (0);
+
+}
+
 }
